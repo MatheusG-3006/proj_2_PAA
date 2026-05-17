4f46ea6a3178cea1c248ed87ec6694383c49ea10
Matheus Gonçalves dos Santos <160793787+MatheusG-3006@users.noreply.github.com>
2026-05-17 20:13:33 +0000

    Alterei algumas funções para funções universais

--- arquivo.c
+++ arquivo.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+ 
 int numFases(FILE *file) {
   char line[256];
   int num = 0;
@@ -14,13 +14,12 @@ int numFases(FILE *file) {
   rewind(file);
   return num;
 }
-
+ 
 void numItem(FILE *file, Fase *fases) {
-
+ 
   char line[256];
   int index = -1;
-  int itemCount = 0;
-
+ 
   while (fgets(line, sizeof(line), file)) {
     if (strncmp(line, "FASE:", 5) == 0) {
       index++;
@@ -32,69 +31,56 @@ void numItem(FILE *file, Fase *fases) {
   }
   rewind(file);
 }
-
+ 
 int lerFases(FILE *file, Fase *fases) {
-
+ 
   if (!file)
     return -1;
-
+ 
   char line[256];
   int numFases = -1;
   int itemCount = 0;
-
+ 
   while (fgets(line, sizeof(line), file)) {
     size_t len = strlen(line);
     if (len > 0 && line[len - 1] == '\n')
       line[len - 1] = '\0';
-
+ 
     if (strlen(line) == 0)
       continue;
-
+ 
     // Queria que switch case funcionasse para string em c
     if (strncmp(line, "FASE:", 5) == 0) {
-     
-    numFases++;
-      sscanf_s(line, "FASE: %s", fases[numFases].nome);
+      numFases++;
+      sscanf(line, "FASE: %s", fases[numFases].nome);
       itemCount = 0;
-      //fases[numFases].numeroItens = 0;
     } else if (strncmp(line, "CAPACIDADE:", 11) == 0) {
-      sscanf_s(line, "CAPACIDADE: %lf", &fases[numFases].capacidade);
+      sscanf(line, "CAPACIDADE: %lf", &fases[numFases].capacidade);
     } else if (strncmp(line, "REGRA:", 6) == 0) {
-      sscanf_s(line, "REGRA: %s", fases[numFases].regra);
+      sscanf(line, "REGRA: %s", fases[numFases].regra);
     } else if (strncmp(line, "ITEM:", 5) == 0) {
       Item item;
       memset(&item, 0, sizeof(Item));
-      
+ 
       // Parse: ITEM: Nome, peso, valor, tipo
       char tempTipo[MAX_TIPO];
-      int matched = sscanf_s(line, "ITEM: %99[^,], %lf, %lf, %s", 
-                             item.nome, 100, &item.peso, &item.valor, tempTipo, MAX_TIPO);
-      
+      int matched = sscanf(line, "ITEM: %99[^,], %lf, %lf, %s",
+                           item.nome, &item.peso, &item.valor, tempTipo);
+ 
       if (matched == 4) {
           item.valorAjust = item.valor;
           strncpy(item.tipo, tempTipo, MAX_TIPO - 1);
           item.tipo[MAX_TIPO - 1] = '\0';
-          
+ 
           fases[numFases].itens[itemCount] = item;
           itemCount++;
       }
     }
   }
-
+ 
   if (itemCount > 0)
     numFases++;
   rewind(file);
-
+ 
   return numFases;
 }
-
-/*
-int main()
-{
-    int num = numFases("entrada.txt");
-    Fase * fase = calloc(num, sizeof(Fase));
-    lerFases("entrada.txt", fase);
-    free(fase);
-    return 0;
-}
-*/
--- main.c
+++ main.c
@@ -1,23 +1,23 @@
-/*
+/* 
 Nome: Isaac Xavier de Araújo RA: 
 Nome: Jean Carlos Antunes Rocha RA:
 Nome: Matheus Gonçalves dos Santos RA: 10439447
 Disciplina: Projeto e Análise de Algoritmos II
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "itens.h"
 #include "guloso.h"
 #include "arquivo.h"
-
-int main(int argc, char *argv[]) {
-    FILE *input;
-    fopen_s(&input, "entrada.txt", "r");
+ 
+int main(void) {
+    FILE *input = fopen("entrada.txt", "r");
     if (!input)
         return 1;
     
     int num = numFases(input);
-    Fase * fase = calloc(num, sizeof(Fase));
+    Fase *fase = calloc(num, sizeof(Fase));
     numItem(input, fase);
     lerFases(input, fase);
     /*
@@ -27,10 +27,8 @@ int main(int argc, char *argv[]) {
             printf("Nome: %s, Preço: %.2lf, Categoria: %s\n", fase[i].itens[j].nome, fase[i].itens[j].valor, fase[i].itens[j].tipo);
         }
     }
-    
     */
-    FILE *output;
-    fopen_s(&output, "saida.txt", "w");
+    FILE *output = fopen("saida.txt", "w");
     for (int i = 0; i < num; i++) {
         resolverFase(fase[i], output);
     }
@@ -39,4 +37,3 @@ int main(int argc, char *argv[]) {
     free(fase);
     return 0;
 }
-
